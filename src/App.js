8bf6e14d1201cd68609df8078ffe6267988a28b2
steven <szapata427@yahoo.com>
2019-08-11 12:39:35 -0400

    added variables to the env file for api keys and working as expected

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 
 
 firebase.initializeApp({
-  apiKey: `${process.env.REACT_FIREBASE_API_KEY}`,
-  authDomain: `${process.env.REACT_FIREBASE_DOMAIN}`
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_DOMAIN}`
+
+  // apiKey: `AIzaSyCioXCjswG0GDdtpiTGUW37yt459cjJO9Y`,
+  // authDomain: `saveyourfuture.firebaseapp.com`
 })
+
 
 
 class App extends Component {
@@ -31,9 +35,13 @@ class App extends Component {
       this.setState({ isSignedIn: !!user })
       console.log("user", user)
     })
+
+    console.log(process.env.REACT_FIREBASE_API_KEY)
+    console.log(process.env.REACT_FIREBASE_DOMAIN)
   }
 
   render() {
+    console.log(this.state.isSignedIn)
     return (
       <div className="App">
         {this.state.isSignedIn ? (
