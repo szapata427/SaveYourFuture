04324b4401dc15596e9f3a84ad4c3d5eea6bfe33
steven <szapata427@yahoo.com>
2019-08-16 00:54:29 -0400

    navbar component was added and css features were added as well

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { Component } from "react"
-import "./App.css"
-import firebase from "firebase"
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
-import LogInHomePage from "./Components/HomeLogin"
-
+import React, { Component } from "react";
+import "./App.css";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import LogInHomePage from "./Components/HomeLogin";
 
 firebase.initializeApp({
   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
   authDomain: `${process.env.REACT_APP_FIREBASE_DOMAIN}`
-
-})
-
-
+});
 
 class App extends Component {
-  state = { 
+  state = {
     isSignedIn: false,
     userDatabase: null
-      }
+  };
   uiConfig = {
     signInFlow: "popup",
     signInOptions: [
@@ -30,67 +26,61 @@ class App extends Component {
     callbacks: {
       signInSuccessWithAuthResult: () => false
     }
-  }
-
-
-
+  };
 
   componentDidMount = () => {
     firebase.auth().onAuthStateChanged(user => {
-      this.setState({ isSignedIn: !!user })
-      console.log("user", user)
-      
-      let userEmail = user.email
-      let emailsearchurl = `http://localhost:5000/saveyourfuture/api/v1.0/SearchUserEmail?email=${userEmail}`
-      console.log(emailsearchurl)
+      this.setState({ isSignedIn: !!user });
+      console.log("user", user);
+
+      let userEmail = user.email;
+      let emailsearchurl = `http://localhost:5000/saveyourfuture/api/v1.0/SearchUserEmail?email=${userEmail}`;
+      console.log(emailsearchurl);
       fetch(emailsearchurl)
-      .then(response => response.json())
-      .then(data => {
-        if (data.result == null) {
-          console.log('user not saved in database, need to save')
-          this.saveUserToDatabase(user)
-        }
-        else {
-          console.log("user is signed in")
-        }
-      })
-    })
+        .then(response => response.json())
+        .then(data => {
+          if (data.result == null) {
+            console.log("user not saved in database, need to save");
+            this.saveUserToDatabase(user);
+          } else {
+            console.log("user is signed in");
+          }
+        });
+    });
+  };
 
-  }
-
-
-  saveUserToDatabase = (userInfo) => {
-    let namearray = userInfo.displayName.split(" ")
-    let arraylength = namearray.length
-    let firstname = namearray[0]
-    let lastname = namearray[arraylength - 1]
-    let email = userInfo.email
+  saveUserToDatabase = userInfo => {
+    let namearray = userInfo.displayName.split(" ");
+    let arraylength = namearray.length;
+    let firstname = namearray[0];
+    let lastname = namearray[arraylength - 1];
+    let email = userInfo.email;
 
     let newUserData = {
       FirstName: firstname,
       LastName: lastname,
       Email: email
-    }
+    };
     fetch(`http://localhost:5000/saveyourfuture/api/v1.0/NewUser`, {
-      method: 'POST',
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json"
       },
       body: JSON.stringify(newUserData)
     })
-    .then(response => response.json())
-    .then(resp => console.log(resp))
-  }
+      .then(response => response.json())
+      .then(resp => console.log(resp));
+  };
 
   render() {
-    console.log(this.state.isSignedIn)
+    console.log(this.state.isSignedIn);
     return (
       <div className="App">
         {this.state.isSignedIn ? (
           <span>
+            <LogInHomePage signedIn={this.state.isSignedIn} />
             <div>Signed In!</div>
-            <LogInHomePage signedIn={this.state.isSignedIn}/>
             <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
             <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
             <img
@@ -100,15 +90,20 @@ class App extends Component {
           </span>
         ) : (
           <React.Fragment>
-            <StyledFirebaseAuth
-              uiConfig={this.uiConfig}
-              firebaseAuth={firebase.auth()}
-            />
+            <div className="login-homepage-options-main-container">
+              <div className="homepage-div-title-main-container">
+                Welcome To Save Your Future
+              </div>
+              <StyledFirebaseAuth
+                uiConfig={this.uiConfig}
+                firebaseAuth={firebase.auth()}
+              />
+            </div>
           </React.Fragment>
         )}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/Components/HomeLogin.js
+++ src/Components/HomeLogin.js
@@ -1,24 +1,32 @@
-import React, {Component} from 'react'
-
-
+import React, { Component } from "react";
 
 class LogInHomePage extends Component {
-    render() {
-        console.log(this.props.signedIn)
-        return (
+  render() {
+    console.log(this.props.signedIn);
+    return (
+      <React.Fragment>
         <div>
-            <ul className="navbar-ul-container-main">
-                <li><a className="active" href="#home">Home</a></li>
-                <li><a href="#news">News</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#about">About</a></li>
-                </ul>
-
-                <div className="loginhomepage-info-div">
-                </div>
+          <ul className="navbar-ul-container-main">
+            <li>
+              <a className="active" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#news">News</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+          </ul>
+          <div className="loginhomepage-info-div" />
         </div>
-    )}
+      </React.Fragment>
+    );
+  }
 }
 
-
-export default LogInHomePage
+export default LogInHomePage;
