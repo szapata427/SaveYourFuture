e5fa1f174930fa98d8741e8461be8b48b83e8f05
steven <szapata427@yahoo.com>
2019-08-23 11:43:51 -0400

    added the action for the delete user

--- src/Store/Actions/UserAction.js
+++ src/Store/Actions/UserAction.js
@@ -4,3 +4,8 @@ export const currentUser = (theuser) => {
     console.log('hit the action for user')
     return {type: "CURRENT_USER", value: theuser}
 }
+
+export const deleteUser = () => {
+    console.log('deleting the user')
+    return {type: "DELETE_USER", value: ""}
+}
