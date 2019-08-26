fca0948209ed16fa67f52cbba215aa1a7e6e8993
steven <szapata427@yahoo.com>
2019-08-25 22:16:01 -0400

    added state for each of the inputs in the form and handlesubmit button

--- src/Components/AddTransactionComponent.js
+++ src/Components/AddTransactionComponent.js
@@ -6,73 +6,125 @@ import { NavLink } from "react-router-dom";
 import { number } from "prop-types";
 
 class AddTransaction extends Component {
+  state = {
+    numOfDecimalsTwo: false,
+    transactionAmount: null,
+    transactionType: null,
+    transactionNotes: null
 
-    state = {
-        transactionAmount: null,
-        numOfDecimalsTwo: false
-    }
+  };
 
-    handleChange = (event) => {
-        console.log(event.target)
-        let htmlname = event.target.name
-        let inputValue = event.target.value
-        let numberDecimals;
-        let inputstring;
-        var inputDecimals;
-        let inputBeforeDecimals;
+  handleChange = event => {
+    console.log(event.target.value);
+    let htmlname = event.target.name;
+    console.log(htmlname)
+    let inputValue = event.target.value;
+    let numberDecimals;
+    let inputstring;
+    let inputDecimals;
+    let inputBeforeDecimals;
+    let hasDecimals;
 
-        if (htmlname == "Amount" && inputValue)  {
-            if (this.state.numOfDecimalsTwo) {
-                inputDecimals = inputValue.toString().split('.')[1].slice(0,2)
-                inputBeforeDecimals = inputValue.toString().split('.')[0]
-                inputValue = parseFloat(inputBeforeDecimals + '.' + inputDecimals)
-            }
-            this.setState({
-                transactionAmount: inputValue
-            })
-            console.log(inputValue)
-            inputValue = parseFloat(inputValue)
-             inputstring = inputValue.toString()
-            var hasDecimals = inputstring.indexOf('.')
-            console.log(hasDecimals)
-            if (hasDecimals != -1) {
-                    var lastTwoDecimals = inputstring.split('.')[1]
-                        if (lastTwoDecimals.length === 2) {
-                            console.log('hit two decimals', lastTwoDecimals)
-                            this.setState({
-                                numOfDecimalsTwo: true
-                            })
-                }
-
-                }
+    if (htmlname == "Amount" && inputValue) {
+      if (this.state.numOfDecimalsTwo) {
+        inputDecimals = inputValue
+          .toString()
+          .split(".")[1]
+          .slice(0, 2);
+        inputBeforeDecimals = inputValue.toString().split(".")[0];
+        inputValue = parseFloat(inputBeforeDecimals + "." + inputDecimals);
+      }
+      this.setState({
+        transactionAmount: inputValue
+      });
+      console.log(inputValue);
+      inputValue = parseFloat(inputValue);
+      inputstring = inputValue.toString();
+      hasDecimals = inputstring.indexOf(".");
+      console.log(hasDecimals);
+      if (hasDecimals != -1) {
+        var lastTwoDecimals = inputstring.split(".")[1];
+        if (lastTwoDecimals.length === 2) {
+          console.log("hit two decimals", lastTwoDecimals);
+          this.setState({
+            numOfDecimalsTwo: true
+          });
         }
+      }
     }
 
-    render() {
-        return (
-            <React.Fragment>
-            <div id="addtransaction-form-main-wrapper-div">
-                <form className="add-transaction-form">
-                <div className="select-options-tranactions-wrapper-div">
-                <select className="select-options-tranactions" onChange={this.handleChange}>
-                <option value="Withdrawl">Withdrawl</option>
-                <option value="Deposit">Deposit</option>
-                </select>
-                </div>
-                    <label class="transaction-label">Amount</label>
-                <input id="transaction-amount-input" value={this.state.transactionAmount} pattern="^\d+(?:\.\d{1,2})?$" placeholder="0.00" 
-                step=".01" type="number" name="Amount" min="0.01" onChange={this.handleChange}/>
-                <br></br> 
-                <label class="transaction-label">Notes</label>
-                <input id="transaction-notes-input" type="text" label="Notes" min="0.01" onChange={this.handleChange}/>
-                <br></br> 
-                </form>
+    if (htmlname == "Notes") {
+        this.setState({
+            transactionNotes: event.target.value
+        })
+    }
+    if (htmlname == "TransactionType") {
+        this.setState({
+            transactionType: event.target.value
+        })
+    }
+
+  };
+
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+      console.log(event)
+      console.log(this.state)
+  }
+
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <div id="addtransaction-form-main-wrapper-div">
+          <form className="add-transaction-form" onSubmit={(e) => this.handleSubmit(e)}>
+            <div className="select-options-tranactions-wrapper-div">
+              <select
+                className="select-options-tranactions"
+                onChange={this.handleChange}
+                name="TransactionType"
+              >
+                <option value="Withdrawl" data-type="negative">
+                  Withdrawl
+                </option>
+                <option value="Deposit" data-type="positive">
+                  Deposit
+                </option>
+              </select>
             </div>
-
-
-            </React.Fragment>
-        )
-    }
+            <label class="transaction-label">Amount</label>
+            <input
+              id="transaction-amount-input"
+              value={this.state.transactionAmount}
+              pattern="^\d+(?:\.\d{1,2})?$"
+              placeholder="0.00"
+              step=".01"
+              type="number"
+              name="Amount"
+              min="0.01"
+              onChange={this.handleChange}
+            />
+            <br></br>
+            <label class="transaction-label">Notes</label>
+            <input
+              id="transaction-notes-input"
+              type="text"
+              min="0.01"
+              onChange={this.handleChange}
+              name="Notes"
+            />
+            <br></br>
+            <input class="add-transaction-submit-button" type="submit" value='Submit'/>
+          </form>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default connect(null, null)(AddTransaction)
+export default connect(
+  null,
+  null
+)(AddTransaction);
