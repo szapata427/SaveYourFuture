14f6af3286b39b8cd7d9ee897c47f013fda129f3
steven <szapata427@yahoo.com>
2019-08-29 13:23:59 -0400

    sending the transactions to the child componenent

--- src/Components/CurrentTransactionsComponent.js
+++ src/Components/CurrentTransactionsComponent.js
@@ -4,15 +4,29 @@ import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { number } from "prop-types";
+import IndividualTransactionComponent from './IndividualTransactionComponent'
 
 
 
 class ShowCurrentTransactions extends Component {
+
+
+    mapFetchTransactions = () => {
+        let transactionsArrayFromProps = this.props.currentTransactions.result
+        return transactionsArrayFromProps.map(trans => {
+            console.log(trans)
+            return <IndividualTransactionComponent key={trans.Id} transaction={trans}/>
+
+        })
+        
+    }
+
+
     render() {
-        console.log(this.props) 
+
         return(
             <React.Fragment>
-
+                {this.props.currentTransactions ? this.mapFetchTransactions() : null}
             </React.Fragment>
         )
     }
@@ -22,9 +36,9 @@ class ShowCurrentTransactions extends Component {
 
 
 const mapStateToProps = (state) => {
-    console.log(state)
+
     return {
-        currentTransactions: state
+        currentTransactions: state.transactions.currentTransactions
     }
 
 }
