2540f6386c1af3eafc71b362c1ea469b34b50c6b
steven <szapata427@yahoo.com>
2019-09-01 16:32:43 -0400

    changed the order in which a transaction is added to state, will go in as first element within array

--- src/Store/Reducers/Transactions.js
+++ src/Store/Reducers/Transactions.js
@@ -7,14 +7,20 @@ const fetchUsersTransactions = (state = initState, action) => {
     console.log(action.value)
     switch(action.type) {
         case "FETCH_USERS_TRANSACTIONS":
+            let fetchedArray = action.value
+            // let filteredByDateDesc = fetchedArray.sort(trans => {
+            //     let firstDate = new Date(trans["CreatedOn"])
+            //     let secondDate = new Date(trans["CreatedOn"])
+            //     return secondDate - firstDate
+            // })
             console.log('chaging state for users transactions in reducer')
-            return {currentTransactions: action.value}
+            return {currentTransactions: fetchedArray}
 
 
 
         case "ADD_NEW_TRANSACTION":
             let newTrans = action.value
-            let newStateArray = [...state.currentTransactions, newTrans]
+            let newStateArray = [newTrans, ...state.currentTransactions]
             console.log(action.value)
             return {currentTransactions: newStateArray}
         }
